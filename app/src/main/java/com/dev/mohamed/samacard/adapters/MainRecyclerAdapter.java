bcfd1f176b68f6df0ea125d15a70d088c7731ce4
mohamed <mohamad1325@gmail.com>
2018-03-09 17:51:11 +0200

    logo is profile picture

--- app/src/main/java/com/dev/mohamed/samacard/adapters/MainRecyclerAdapter.java
+++ app/src/main/java/com/dev/mohamed/samacard/adapters/MainRecyclerAdapter.java
@@ -26,10 +26,10 @@ import butterknife.ButterKnife;
 public class MainRecyclerAdapter extends RecyclerView.Adapter<MainRecyclerAdapter.MainRecycelerAdapterViewholder>{
 
 
-    String [] images= {"http://brandmark.io/logo-rank/random/pepsi.png"
-            ,"https://upload.wikimedia.org/wikipedia/commons/4/4c/Logo-Free.jpg"
-            ,"https://www.freelogodesign.org/img/logo-ex-7.png"
-            ,"https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png"};
+    String [] images= {"https://scontent-cai1-1.xx.fbcdn.net/v/t1.0-9/18485817_1741335819215943_5407189630743605678_n.jpg?oh=3aefc25f3cddfbf88f01128d6d5f4e05&oe=5B3BE923"
+            ,"https://scontent-cai1-1.xx.fbcdn.net/v/t1.0-9/21077517_1575516205840605_3574721175345761590_n.jpg?oh=9078b01c7154ce3fd380f9aab52b14b3&oe=5B436D0D"
+            ,"https://scontent-cai1-1.xx.fbcdn.net/v/t1.0-9/14516611_570362563148747_2537644147402450349_n.jpg?oh=3bea032219763f5239d935bd7a78093d&oe=5B4A52B4"
+            ,"https://scontent-cai1-1.xx.fbcdn.net/v/t1.0-9/20663974_1420864141324114_7824265645260496196_n.jpg?oh=17b77b029c750fcd14d0508130c8a749&oe=5B0B9592"};
 
     String [] names={"Mohamed Ragab","Ahmed bo7sen","Mahmoud ahmed","Mos3ad ebrahem","Saud elsayed"};
     Context context;
--- app/src/main/java/com/dev/mohamed/samacard/card/CardActivity.java
+++ app/src/main/java/com/dev/mohamed/samacard/card/CardActivity.java
@@ -2,15 +2,25 @@ package com.dev.mohamed.samacard.card;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.dev.mohamed.samacard.R;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class CardActivity extends AppCompatActivity {
 
+    @BindView(R.id.img_logo)ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
+
+        ButterKnife.bind(this);
+        Picasso.with(this).load("https://scontent-cai1-1.xx.fbcdn.net/v/t1.0-9/18485817_1741335819215943_5407189630743605678_n.jpg?oh=3aefc25f3cddfbf88f01128d6d5f4e05&oe=5B3BE923").into(logo);
+
     }
 }
